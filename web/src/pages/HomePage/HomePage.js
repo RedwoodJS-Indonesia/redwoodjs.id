ff1a9585cfb7a76a12b57436d0ccb6450b57e235
ekaone <ekaone3033@gmail.com>
2020-05-28 21:02:07 +0700

    install tailwind

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -1,49 +1,45 @@
 import React from 'react'
-import rw from './picture.png'
+import rw from './diecut_mark.png'
 
 const HomePage = () => {
   return (
     <>
-      <div className="container">
-        <div className="pt-5">
-          <center>
-            <img src={rw} alt="Responsive image" />
-          </center>
-        </div>
-        <div className="pt-4 d-flex justify-content-center">
-          <h1>RedwoodJS Indonesia</h1>
-        </div>
-        <div className="pt-4">
-          <div className="border border-danger">
-            <div className="p-2">
-              <div className="card bg-dark">
-                <div className="card-body text-light">
-                  <blockquote className="twitter-tweet">
-                    <p lang="en" dir="ltr">
-                      Redwood is React + GraphQL + Prisma2 + Babel + webpack +
-                      CDN + Functions + DB (+ Jest, Storybook, &amp; others
-                      soon). I see so much untapped potential in this mix of
-                      technologies. Let’s unlock it together! Follow{' '}
-                      <a href="https://twitter.com/redwoodjs?ref_src=twsrc%5Etfw">
-                        @redwoodjs
-                      </a>{' '}
-                      for updates.
-                    </p>
-                    &mdash; Tom Preston-Werner (@mojombo){' '}
-                    <a href="https://twitter.com/mojombo/status/1237441122881794048?ref_src=twsrc%5Etfw">
-                      March 10, 2020
-                    </a>
-                  </blockquote>{' '}
-                  <script
-                    async
-                    src="https://platform.twitter.com/widgets.js"
-                    charSet="utf-8"
-                  ></script>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+      <div className="pt-5">
+        <center>
+          <img
+            src={rw}
+            alt="Responsive image"
+            style={{ height: '200px', width: '200px' }}
+          />
+        </center>
+      </div>
+      <div className="pt-4 d-flex justify-content-center">
+        <h1 className="flex justify-center text-5xl text-red-700">
+          RedwoodJS Indonesia
+        </h1>
+      </div>
+      <div className="mt-2 p-6 mx-auto flex max-w-xl rounded shadow-lg bg-red-800 text-white">
+        <blockquote className="twitter-tweet">
+          <p lang="en" dir="ltr">
+            Redwood is React + GraphQL + Prisma2 + Babel + webpack + CDN +
+            Functions + DB (+ Jest, Storybook, &amp; others soon). I see so much
+            untapped potential in this mix of technologies. Let’s unlock it
+            together! Follow{' '}
+            <a href="https://twitter.com/redwoodjs?ref_src=twsrc%5Etfw">
+              @redwoodjs
+            </a>{' '}
+            for updates.
+          </p>
+          &mdash; Tom Preston-Werner (@mojombo){' '}
+          <a href="https://twitter.com/mojombo/status/1237441122881794048?ref_src=twsrc%5Etfw">
+            March 10, 2020
+          </a>
+        </blockquote>{' '}
+        <script
+          async
+          src="https://platform.twitter.com/widgets.js"
+          charSet="utf-8"
+        ></script>
       </div>
     </>
   )
